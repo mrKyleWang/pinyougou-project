dcd053d8258cbc84b5b7dc01b59377a5516d0d29
KyleWang <Kyle.Wang.cn@outlook.com>
2018-03-08 21:52:31 +0800

    前台结果封装

---
+++ pinyougou-pojo/src/main/java/com/pinyougou/entity/Result.java
@@ -0,0 +1,33 @@
+package com.pinyougou.entity;
+
+/**
+ * 页面结果封装
+ * @author Kyle.Wang
+ * 2018-03-08 21:46
+ */
+public class Result {
+
+    private boolean success;
+    private String message;
+
+    public Result(boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
