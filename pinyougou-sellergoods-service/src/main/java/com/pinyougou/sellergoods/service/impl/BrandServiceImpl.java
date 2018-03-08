e22f447fd67d2eeb2571be04387d00a4a36b1ce4
KyleWang <Kyle.Wang.cn@outlook.com>
2018-03-08 20:27:41 +0800

    update

--- pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
+++ pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
@@ -1,13 +1,12 @@
 package com.pinyougou.sellergoods.service.impl;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
 import com.alibaba.dubbo.config.annotation.Service;
 import com.pinyougou.mapper.TbBrandMapper;
 import com.pinyougou.pojo.TbBrand;
 import com.pinyougou.sellergoods.service.BrandService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 @Service
 public class BrandServiceImpl implements BrandService {
 
